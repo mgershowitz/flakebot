a9eb6a1b7848a0178b236142912c4da07e53eb28
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-08-15 13:29:17 -0400

    add console.log err)

--- models/events.js
+++ models/events.js
@@ -37,7 +37,7 @@ module.exports = {
       },
       (err,result,body)=>{
         if (err) throw err;
-        // console.log(result.body)
+        console.log(err)
         res.rows = result.body
         next()
       })

458db4a6538c94ddac5d66497c3ae9d1610561c6
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-08-23 15:49:50 -0400

    test

--- src/client/app/main.jsx
+++ src/client/app/main.jsx
@@ -4,4 +4,4 @@ require('bootstrap/dist/css/bootstrap.css');
 require('../css/styles.css')
 require('../css/animate.css')
 require('../css/reactToastr.css')
-console.log('working')
+console.log('workin')

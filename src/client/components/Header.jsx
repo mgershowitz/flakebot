16be9d3187cb00b98035c2cc1d8d4410ef8d7117
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-01-31 17:33:44 -0500

    test change

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ajax  from '../helpers/ajaxAdapter.js'
 import styles from '../css/headerStyles.css'
 const jwtDecode = require('jwt-decode');
-// const flakeBot = require('../../../models/flakebot')
 
-export default class Header extends React.Component {
+export default class Header extends Component {
   handleSubmit(event){
-  event.preventDefault()
-  let thing = event.target
-  let user = {
-    username: event.target.username.value,
-    password: event.target.password.value
-  }
-  ajax.loginUser(user).then( user => {
-      console.log(user)
-      localStorage.setItem('token', user.token)
-      localStorage.setItem('user_id', jwtDecode(user.token).user_id)
-      console.log(jwtDecode(user.token).user_id)
-      if(user.success){
-        this.props.userLoggedIn()
-      } else {
-        thing.reset()
-      }
+    event.preventDefault()
+    let thing = event.target
+    let user = {
+      username: event.target.username.value,
+      password: event.target.password.value
     }
+    ajax.loginUser(user).then( user => {
+        console.log(user)
+        localStorage.setItem('token', user.token)
+        localStorage.setItem('user_id', jwtDecode(user.token).user_id)
+        console.log(jwtDecode(user.token).user_id)
+        if(user.success){
+          this.props.userLoggedIn()
+        } else {
+          thing.reset()
+        }
+      }
     )
   }
 
@@ -39,96 +38,40 @@ export default class Header extends React.Component {
     this.props.userLoggedOut()
   }
 
-  myEvents(event){
-    event.preventDefault()
-    this.props.showUserEvents()
-    event.target.reset()
+  render(){
+      return (
+        <header className={styles['header']}>
+          <h1 className={styles['title']}>FlakeBot</h1>
+          { !this.props.user &&
+            <div className={styles["fBNoUser"]}>
+              <img src="http://www.videogamesprites.net/Earthbound/Party/Ness/Ness%20(Robot)%20-%20Walk%20(SW).gif" alt=""/>
+            </div>
+            <form action="post" className={styles["signIn"]} onSubmit={this.handleSubmit.bind(this)}>
+              <span className={styles['grid']}>
+                <div>
+                  <label className={styles['username']} htmlFor="username">username</label>
+                  <label className={styles['password']} htmlFor="password">password</label>
+                </div>
+                <div>
+                  <input type="text" name="username" placeholder="enter username"/>
+                  <input type="password" name="password" placeholder="enter password"/>
+                  <button>Sign In</button>
+                </div>
+              </span>
+            </form> }
+            { this.props.user &&
+              <div className={styles["fBNoUser"]}>
+                <img src="http://www.videogamesprites.net/Earthbound/Party/Ness/Ness%20(Robot)%20(Front).gif" alt=""/>
+              </div>
+              <span className={styles['signedIn']}>
+                <button onClick={this.bro.bind(this)}>Talk to FlakeBot</button>
+                <button onClick={this.myEvents.bind(this)}>My Events</button>
+                <button onClick={this.clearLocalStorage.bind(this)}>Log Out</button>
+              </span> }
+        </header>
+      )
+    }
   }
 
-render(){
-    return (
-      <header className={styles['header']}>
-        <h1 className={styles['title']}>FlakeBot</h1>
-        <div className={styles["fBNoUser"]}>
-          <img src="http://www.videogamesprites.net/Earthbound/Party/Ness/Ness%20(Robot)%20-%20Walk%20(SW).gif" alt=""/>
-        </div>
-        <form action="post" className={styles["signIn"]} onSubmit={this.handleSubmit.bind(this)}>
-          <span className={styles['grid']}>
-            <div>
-              <label className={styles['username']} htmlFor="username">username</label>
-              <label className={styles['password']} htmlFor="password">password</label>
-            </div>
-            <div>
-              <input type="text" name="username" placeholder="enter username"/>
-              <input type="password" name="password" placeholder="enter password"/>
-              <button>Sign In</button>
-            </div>
-          </span>
-        </form>
-      </header>
-    )
-  }
-}
-  // render(){
-  //   if(!this.props.user){
-  //   return (
-  //     <div className={styles[".header">
-  //       <h1>FlakeBot</h1>
-  //       <img className=".fBNoUser" src="http://www.videogamesprites.net/Earthbound/Party/Ness/Ness%20(Robot)%20-%20Walk%20(SW).gif" alt=""/>
-  //       <div className='.login'>
-  //         <form action="post" className="signIn" onSubmit={this.handleSubmit.bind(this)}>
-  //           <table>
-  //             <tbody>
-  //               <tr>
-  //                 <td>
-  //                   <label htmlFor="username">username</label>
-  //                 </td>
-  //                 <td>
-  //                   <label htmlFor="password">password</label>
-  //                 </td>
-  //               </tr>
-  //               <tr>
-  //                 <td>
-  //                   <input type="text" name="username" placeholder="enter username"/>
-  //                 </td>
-  //                 <td>
-  //                   <input type="password" name="password" placeholder="enter password"/>
-  //                 </td>
-  //                 <td>
-  //                   <button>Sign In</button>
-  //                 </td>
-  //               </tr>
-  //             </tbody>
-  //           </table>
-
-  //         </form>
-  //       </div>
-  //     </div>
-  //   )
-  // } else {
-  //   return (
-  //     <div className=".headerUser">
-  //       <h1>FlakeBot</h1>
-  //       <img className=".fBUser" src="http://www.videogamesprites.net/Earthbound/Party/Ness/Ness%20(Robot)%20(Front).gif" alt=""/>
-
-  //         <table className='.signedIn'>
-  //           <tbody>
-  //             <tr>
-  //               <td>
-  //                 <button onClick={this.bro.bind(this)}>Talk to FlakeBot</button>
-  //               </td>
-  //               <td>
-  //                 <button onClick={this.myEvents.bind(this)}>My Events</button>
-  //               </td>
-  //               <td>
-  //                 <button onClick={this.clearLocalStorage.bind(this)}>Log Out</button>
-  //               </td>
-  //             </tr>
-  //           </tbody>
-  //         </table>
-  //     </div>
-  //   )
-  // }
-  // }
 
 
--- src/client/components/main.jsx
+++ src/client/components/main.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router';
 import Footer from './Footer.jsx';
 import Header from './Header.jsx';
 import myEvents from '../data/events';
+import ajax  from '../helpers/ajaxAdapter.js'
+
 
 export default class Main extends Component {
   constructor(){
@@ -22,15 +24,210 @@ export default class Main extends Component {
       flakeBot: false,
       notification: false
     }
-    // this.handleUpdateSearch = this.handleUpdateSearch.bind(this)
-
   }
 
+  componentDidMount(){
+    if(localStorage.token){
+      ajax.getMyEvents(localStorage.user_id).then(myEvents => {
+        this.setState({
+          userEvents: myEvents,
+          user:true
+        })
+      })
+    }
+  }
+
+  handleUpdateLocationSearch(event){
+    this.setState({
+      location: event.target.value,
+    })
+  }
+
+  handleUpdateKeywordSearch(event){
+    this.setState({
+      keyword: event.target.value
+    })
+  }
+
+  handleUpdateTimeSearch(event){
+    this.setState({
+      time: event.target.value
+    })
+  }
+
+  handleSubmitSearch(event){
+  event.preventDefault();
+  ajax.eventsCall(this.state.location,this.state.time).then(res => {
+        this.setState({
+        results: res.event,
+        searched: true,
+        savedSelected:false
+      })
+    })
+  event.target.reset()
+  }
+
+ selectEventDetail(event){
+    event.preventDefault();
+    ajax.eventDetailCall(event.target.alt).then( event =>{
+      console.log(event)
+      this.setState({
+        singleResult: event,
+        selected: true,
+        searched:false
+      })
+    })
+  }
+
+ selectSavedEventDetail(event){
+  event.preventDefault();
+  ajax.eventDetailCall(event.target.alt).then( event =>{
+    // console.log(event)
+    this.setState({
+      singleResult: event,
+      selected: false,
+      searched:false,
+      savedSelected:true,
+      results: this.state.userEvents
+    })
+  })
+}
+
+  returnToSearch(event){
+    event.preventDefault();
+    this.setState({
+      selected: false,
+      searched: true
+    })
+  }
+
+  displayUserEvents(){
+    this.setState({
+      searched:false,
+      selected:false,
+      savedSelected:false
+    })
+  }
+
+  saveEvent(event){
+    event.preventDefault()
+    let newEvent = {
+      title: event.target.title.value,
+      event_id: event.target.event_id.value,
+      image: event.target.image.value,
+      event_time: event.target.event_time.value
+    }
+    ajax.addUserEvent(newEvent).then(event => {
+      console.log("success!!")
+      ajax.getMyEvents(localStorage.user_id).then( myEvents => {
+        this.setState({
+          userEvents: myEvents,
+          user:true,
+          searched: false,
+          selected: false
+        })
+      })
+    })
+  }
+
+  deleteEvent(event){
+    event.preventDefault()
+    let userEvent = {event_id: event.target.event_id.value}
+    console.log(userEvent)
+    ajax.deleteUserEvent(userEvent).then(event => {
+      console.log("success!!")
+      ajax.getMyEvents(localStorage.user_id).then( myEvents => {
+        this.setState({
+          userEvents: myEvents,
+          user: true,
+          savedSelected: false
+        })
+      })
+    })
+  }
+
+  createUser(event){
+    event.preventDefault();
+    let thing = event.target
+    let newUser = {
+      username: event.target.username.value,
+      email: event.target.email.value,
+      phone: event.target.phone.value,
+      password: event.target.password.value
+    }
+    let user = {
+      username: newUser.username,
+      password: newUser.password
+    }
+    console.log(user)
+    ajax.createNewUser(newUser).then(
+      setTimeout(()=>{
+        ajax.loginUser(user).then( user => {
+        console.log(user)
+        localStorage.setItem('token', user.token)
+        localStorage.setItem('user_id', jwtDecode(user.token).user_id)
+        console.log(jwtDecode(user.token).user_id)
+        if(user.success){
+          this.userLogIn()
+        } else {
+          thing.reset()
+        }
+      })}, 500)
+      )
+    }
+
+  userLogIn(){
+    ajax.getMyEvents(localStorage.user_id).then( myEvents => {
+      // console.log(myEvents)
+      this.setState({
+        userEvents: myEvents,
+        user:true,
+      })
+    })
+  }
+
+  userLogOut(){
+    this.setState({
+      user:false,
+      searched:false,
+      selected:false,
+      savedSelected:false,
+      userEvents:[],
+      results: []
+    })
+  }
+
+  notificationOn(){
+    this.setState({
+      notification:true
+    })
+  }
+
+  closeNotification(){
+    this.setState({
+      notification:false
+    })
+  }
+
+  addAlert () {
+    this.refs.container.success(
+      "Let's see if he's coming to your next event.",
+      "Your message has been sent to FlakeBot!!", {
+      timeOut: 4000,
+      extendedTimeOut: 4000
+    });
+  }
 
   render() {
     return (
       <div>
-        <Header />
+        <Header
+        user={this.state.user}
+        userLoggedIn={this.userLogIn.bind(this)}
+        userLoggedOut={this.userLogOut.bind(this)}
+        showUserEvents={this.displayUserEvents.bind(this)}
+        onAddAlert={this.addAlert.bind(this)} />
+
         {this.props.children && React.cloneElement(this.props.children, {
           state:this.state
         })}
